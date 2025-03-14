45e783609c374af990fa3a9ed4bff776483958ec
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-03-14 19:27:46 -0400

    Successfully setup neon postgres db and connected it to frontend via drizzle ORM. Created db schema for users table and added test row to table. On home page successfully queried db and pulled users data from db. Added script to package json for generating sql and migrating db to update schemas for future changes.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,13 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
 import { sampleBooks } from "@/lib/constants";
 
-export default function Home() {
+export default async function Home() {
+	const result = await db.select().from(users);
+	console.log(result);
+
 	return (
 		<>
 			<BookOverview currentBook={sampleBooks[0]} />
