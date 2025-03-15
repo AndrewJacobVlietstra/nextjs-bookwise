018a514eff3689fd0c5766e1d53ee742b265bac5
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-03-15 00:35:07 -0400

    Setup next-auth, setup credentials provider to check if user exists based on email and if password is correct. Setup callbacks to update the json web token with user id and name, as well as the session. Session will use JWT strategy.

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,8 @@
 import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
-import { db } from "@/database/drizzle";
-import { users } from "@/database/schema";
 import { sampleBooks } from "@/lib/constants";
 
 export default async function Home() {
-	const result = await db.select().from(users);
-	console.log(result);
-
 	return (
 		<>
 			<BookOverview currentBook={sampleBooks[0]} />
---
+++ lib/auth.ts
@@ -0,0 +1,62 @@
+import NextAuth, { User } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import { compare } from "bcryptjs";
+import { users } from "@/database/schema";
+import { db } from "@/database/drizzle";
+import { eq } from "drizzle-orm";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+	pages: {
+		signIn: "/signin",
+	},
+
+	providers: [
+		Credentials({
+			async authorize(credentials) {
+				if (!credentials?.email || !credentials?.password) return null;
+
+				const [user] = await db
+					.select()
+					.from(users)
+					.where(eq(users.email, credentials.email.toString()))
+					.limit(1);
+
+				if (!user) return null;
+
+				const isPasswordValid = await compare(
+					credentials.password.toString(),
+					user.password
+				);
+
+				if (!isPasswordValid) return null;
+
+				return {
+					id: user.id,
+					email: user.email,
+					name: user.fullName,
+				} as User;
+			},
+		}),
+	],
+	callbacks: {
+		async jwt({ token, user }) {
+			if (user) {
+				token.id = user.id;
+				token.name = user.name;
+			}
+
+			return token;
+		},
+		async session({ token, session }) {
+			if (session.user) {
+				session.user.id = token.id as string;
+				session.user.name = token.name;
+			}
+
+			return session;
+		},
+	},
+	session: {
+		strategy: "jwt",
+	},
+});
