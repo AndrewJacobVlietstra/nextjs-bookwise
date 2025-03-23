a42cff9f8ebe939d3ec48437d09e5a19e29d105a
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-03-22 21:39:09 -0400

    Added bookSchema object to zod validations.ts file

--- lib/validations.ts
+++ lib/validations.ts
@@ -12,3 +12,19 @@ export const signInSchema = z.object({
 	email: z.string().email("Must be of type email."),
 	password: z.string().min(8, "Password must at least have 8 characters."),
 });
+
+export const bookSchema = z.object({
+	id: z.coerce.number(),
+	title: z.string().min(1).max(200),
+	author: z.string().min(1).max(200),
+	genre: z.string().min(1).max(80),
+	rating: z.coerce.number(),
+	totalCopies: z.coerce.number(),
+	availableCopies: z.coerce.number(),
+	description: z.string().min(1).max(2000),
+	coverColor: z.string().min(1).max(500),
+	coverUrl: z.string().min(1).max(500).url(),
+	videoUrl: z.string().min(1).max(500).url(),
+	summary: z.string().min(1).max(2000),
+	isLoanedBook: z.boolean().optional(),
+});
