4ba4ef93859f5320afc42bb71e3d07fb0f8effc9
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-03-28 23:40:55 -0400

    Added capitalize string utility function to utils.ts file

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,6 +5,10 @@ export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs));
 }
 
+export const capitalizeString = (str: string) => {
+	return str[0].toUpperCase() + str.slice(1);
+};
+
 export const getInitials = (name: string) => {
 	return name
 		.split(" ")
