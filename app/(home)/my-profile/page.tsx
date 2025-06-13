7d5a1aed834e1978e56ded3c38d803269c786fc5
Andrew Vlietstra <vlietstra123@hotmail.com>
2025-06-13 12:10:07 -0400

    Created dummybooks data to seed db with, removed samplebooks from constants.

--- app/(home)/my-profile/page.tsx
+++ app/(home)/my-profile/page.tsx
@@ -1,15 +1,30 @@
 import BookList from "@/components/BookList";
 import SignOutButton from "@/components/SignOutButton";
-import { sampleBooks } from "@/lib/constants";
+import { db } from "@/database/drizzle";
+import { desc } from "drizzle-orm";
+import { books } from "@/database/schema";
+import { imageKitUrlEndpoint } from "@/lib/constants";
 
-export default function MyProfilePage() {
+export default async function MyProfilePage() {
+	const latestBooks = await db
+		.select()
+		.from(books)
+		.limit(10)
+		.orderBy(desc(books.createdAt))
+		.then((books) =>
+			books.map((book) => ({
+				...book,
+				coverUrl: `${imageKitUrlEndpoint}${book.coverUrl}`,
+				rating: +book.rating,
+			}))
+		);
 	return (
 		<>
 			<form className="mb-10">
 				<SignOutButton />
 			</form>
 
-			<BookList books={sampleBooks} title="Borrowed Books" />
+			<BookList books={latestBooks} title="Borrowed Books" />
 		</>
 	);
 }
